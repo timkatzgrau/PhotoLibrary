29281ea9217da5e84eb90d485f23b27d384da901
Tim Katzgrau <Katzgrau@nbp-223-110.nbp.ruw.rutgers.edu>
2017-11-22 18:28:39 -0500

    Removed stock user

--- Photos/src/view/ListController.java
+++ Photos/src/view/ListController.java
@@ -71,6 +71,7 @@ public class ListController {
 					   File file3 = new File(userDir+"/Photos/src/stock/stock3.jpeg");
 					   File file4 = new File(userDir+"/Photos/src/stock/stock4.jpeg");
 					   File file5 = new File(userDir+"/Photos/src/stock/stock5.jpg");
+					   
 					   if(!file.exists()) {
 						   file = new File(userDir+"/src/stock/stock1.jpg");
 					   }
